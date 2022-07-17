4589835a386c95be81d874c3037f1f5a39d32038
WithHades <244036962@qq.com>
2022-07-17 17:45:02 +0800

    complete download

--- cocoapods-downloader/downloader.rb
+++
@@ -1,7 +0,0 @@
-require './cocoapods-downloader'
-
-target_path = './'
-options = { :svn => 'svn://svn.woxiu.com/ios/iwanpa/Pods/CommonComponent'}
-options = Pod::Downloader.preprocess_options(options)
-downloader = Pod::Downloader.for_target(target_path, options)
-downloader.download
--- download_library.py
+++ download_library.py
@@ -2,13 +2,6 @@ import json
 import os
 import subprocess
 
-import requests
-
-
-def run_command(cmd):
-    return None
-    return subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='gbk').communicate()[0]
-
 
 # print and log
 def pl(f, msg):
@@ -24,51 +17,57 @@ log_f = open("./log.log", "w", encoding="UTF-8")
 lib_path = "../libraries"
 if not os.path.exists(lib_path):
     os.mkdir(lib_path)
-os.chdir(lib_path)
 
 for lib_name in libs_info:
     # in most cases, the last one is the latest version
     lib_version = list(libs_info[lib_name].keys())[-1]
     lib_info = libs_info[lib_name][lib_version]
     if "source" not in lib_info:
-        pl(lib_name + ":" + lib_version + " does not have a source")
+        pl(lib_name + ":" + lib_version + " does not have a source key.")
         continue
-
     source = lib_info["source"]
-    if "git" in source:
-        git = source["git"]
-        file_path = git[git.rfind("/") + 1 : git.rfind(".")]
-        if file_path == "":
-            pl(log_f, lib_name + ":" + lib_version + " does not find the git file path")
-            continue
-        if os.path.exists(file_path):
-            continue
-        if "tag" in source:
-            run_command("git clone -b {} --depth=1 {}".format(source["tag"], git))
-        else:
-            run_command("git clone {}".format(git))
-        if not os.path.exists(file_path):
-            pl(log_f, lib_name + ":" + lib_version + " downloaded failed from " + git)
+    file_path = lib_name + "_" + lib_version
+    file_path = os.path.join(lib_path, file_path)
+
+    # if file_path is exists and is not empty, then continue
+    if os.path.exists(file_path) and os.listdir(file_path):
+        pl(lib_name + ":" + lib_version + " has already been built.")
         continue
 
-    if "http" in source:
-        url = source["http"]
-        file_path = url[url.rfind("/") + 1:]
-        if os.path.exists(file_path):
+    with open("./cocoapods-downloader/downloader.rb", "w", encoding="UTF-8") as f:
+        # require './cocoapods-downloader'
+
+        # target_path = './'
+        # options = { :git => 'https://github.com/admost/AMR-IOS-ADAPTER-MINTEGRAL.git'}
+        # options = Pod::Downloader.preprocess_options(options)
+        # downloader = Pod::Downloader.for_target(target_path, options)
+        # downloader.download
+        f.write("require './cocoapods-downloader'")
+        f.write("target_path = '{}'".format(file_path))
+        options = "{"
+        for key in source:
+            options += ":" + key + " => '" + source[key] + "'" + ","
+        options = options[:-1] + "}"
+        if len(options) > 4:
+            pl(log_f, lib_name + ":" + lib_version + " parse source file error! " + url)
+            pl(log_f, json.dumps(source))
             continue
-        continue
-        try:
-            res = requests.get(url)
-            if res.status_code != 200:
-                pl(log_f, lib_name + ":" + lib_version + " downloaded failed from " + url)
-                continue
-            with open(file_path, 'wb') as f:
-                f.write(res.content)
-        except Exception as e:
-            pass
-        if not os.path.exists(file_path):
-            pl(log_f, lib_name + ":" + lib_version + " downloaded failed from " + url)
-        continue
-    print(source, 33333)
+        f.write("options = {}".format(options))
+        f.write("options = Pod::Downloader.preprocess_options(options)")
+        f.write("downloader = Pod::Downloader.for_target(target_path, options)")
+        f.write("downloader.download")
+
+    path = os.getcwd()
+    os.chdir("./cocoapods-downloader")
+    cmd = "ruby downloader.rb"
+    subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='gbk')
+    os.chdir(path)
+
+    if os.path.exists(file_path) and os.listdir(file_path):
+        pl(lib_name + ":" + lib_version + " has been downloaded!")
+    else:
+        pl(lib_name + ":" + lib_version + " download failed!")
+
+
 
 
