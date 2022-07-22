0b2c6b38c0aa5a31ccb3ed20f7cb83ecc46bcd57
WithHades <244036962@qq.com>
2022-07-22 14:02:03 +0800

    change the db info

--- upload2db.py
+++ upload2db.py
@@ -3,7 +3,7 @@ import os
 
 import pymongo
 
-client = pymongo.MongoClient("mongodb://lib:%s@code-analysis.org" % os.environ.get("MONGO"))
+client = pymongo.MongoClient("mongodb://%s:%s@code-analysis.org:%s" % (os.environ.get("MONGOUSER"), os.environ.get("MONGOPASS"), os.environ.get("MONGOPORT")))
 db = client["lib"]
 collections = db["lib_source_info"]
 
@@ -12,5 +12,9 @@ with open("./libs_info.json", "r", encoding="UTF-8") as f_:
     for lib_name in libs_info:
         for lib_version in list(libs_info[lib_name].keys()):
             lib_info = libs_info[lib_name][lib_version]
-            collections.update_one(lib_info, {"$set": lib_info}, True)
+            try:
+                ret = collections.update_one(lib_info, {"$set": lib_info}, True)
+            except Exception as e:
+                print(e)
+                print(lib_info)
 
