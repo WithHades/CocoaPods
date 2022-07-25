7d4bca0ea4672cca1ca0df9a9890adc70332e4ef
WithHades <244036962@qq.com>
2022-07-25 15:46:11 +0800

    param the remove

--- parse_oc.py
+++ parse_oc.py
@@ -224,6 +224,11 @@ def main():
 
     logger = utils.config_log(name=__name__, level=logging.INFO, log_path="./logs/{}.log".format(os.path.abspath(__file__)))
     lib_source_info, feature_string, feature_method, feature_lib = get_db_collecttions()
+    if len(sys.argv) >= 3 and sys.argv[2] == "remove":
+        feature_string.remove()
+        feature_method.remove()
+        feature_lib.remove()
+
     global_vals = global_var(logger, lib_source_info, feature_string, feature_method, feature_lib, compiler=sys.argv[1])
 
     os.chdir(lib_path)
@@ -241,3 +246,6 @@ def main():
             continue
         parse_source_info(lib_name, lib_version, ret, global_vals)
 
+
+if __name__ == '__main__':
+    main()
