d2d6a4244d2eafa24237801d51cd28d343134b25
WithHades <244036962@qq.com>
2022-08-02 17:26:54 +0800

    set weight

---
+++ set_weight.py
@@ -0,0 +1,66 @@
+import os
+
+import pymongo
+
+import mongo
+if os.path.exists("secret.py"):
+    import secret
+
+
+def get_db_collecttions():
+    client = pymongo.MongoClient("mongodb://%s:%s@code-analysis.org:%s" % (os.environ.get("MONGOUSER"), os.environ.get("MONGOPASS"), os.environ.get("MONGOPORT")))
+    db = client["lib"]
+    feature_string = db["feature_string"]
+    feature_method = db["feature_method"]
+    feature_lib = db["feature_lib"]
+    feature_weight = db["feature_weight"]
+    return feature_string, feature_method, feature_lib, feature_weight
+
+
+def main():
+    feature_string, feature_method, feature_lib, feature_weight = get_db_collecttions()
+    for lib in feature_lib.find():
+        def get_weights(field, lib, collection):
+            base_query = {"name": lib["name"], "version": lib["version"]}
+            if "subspecs_name" in lib:
+                base_query.update({"subspecs_name": lib["subspecs_name"]})
+            if field not in lib:
+                return {}
+            # 根据方法在整个数据库中出现的次数进行分组
+            count_col = {}
+            for field_ in lib[field]:
+                field_info = collection.find_one({field: field_})
+                if field_info is None:
+                    print(field_, base_query)
+
+                length = len(field_info["library"])
+                if length not in count_col:
+                    count_col[length] = []
+                count_col[length].append(field_info[field])
+
+            # 组间根据频率设置权重
+            lengths = list(count_col.keys())
+            weight_lengths = [1/length for length in lengths]
+            sums_ = sum(weight_lengths)
+            weight_lengths = [length/sums_ for length in weight_lengths]
+
+            weight_fields = {}
+            for length, weight_length in zip(lengths, weight_lengths):
+                # 计算组内权重
+                field_weight = weight_length / len(count_col[length])
+                for field_ in count_col[length]:
+                    print(field_, field_weight)
+                    weight_fields[field_] = field_weight
+
+            base_query.update({field: weight_fields})
+            feature_weight.insert_one(base_query)
+
+        get_weights("method", lib, feature_method)
+        get_weights("string", lib, feature_string)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
