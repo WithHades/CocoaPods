2951b8ce1c7b859e20daa516cba35d0301665501
WithHades <244036962@qq.com>
2022-08-03 16:59:18 +0800

    opt

--- collect_library.py
+++ collect_library.py
@@ -5,43 +5,34 @@ import utils
 from mongo import mongodb
 
 
-def find_specs(path: str, lib_source_info, logger, depth=0):
+def find_specs(path: str, lib_source_info, logger):
     """
     iterate all the specs and collection the information.
     :param path:
     :param lib_source_info: collection
     :param logger:
-    :param depth: path depth.
     :return:
     """
-    if depth <= 4:
-        for sub_path in os.listdir(path):
-            sub_path = os.path.join(path, sub_path)
-            if os.path.isfile(sub_path):
-                continue
-            find_specs(sub_path, lib_source_info, logger, depth + 1)
-        return
 
-    for lib_resource in os.listdir(path):
-        if not lib_resource.endswith('.podspec.json'):
-            continue
-        lib_resource = os.path.join(path, lib_resource)
-        try:
-            with open(lib_resource, "r", encoding="UTF-8") as f:
-                data = f.read().replace("${", "_{").replace("$(", "_(")
-                lib_info = json.loads(data)
-            if "name" in lib_info and "version" in lib_info:
-                lib_name = lib_info["name"]
-                lib_version = str(lib_info["version"])
-            else:
-                sp = lib_resource.split(os.sep)
-                lib_name = sp[-3]
-                lib_version = sp[-2]
-            logger.info("update! lib_name: %s, lib_version: %s." % (lib_name, lib_version))
-            base_query = {"name": lib_name, "version": lib_version}
-            lib_source_info.update_one(base_query, {"$set": lib_info}, True)
-        except Exception as e:
-            logger.error("A error occurred while updating lib_source_info: %s" % e.args[0])
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            lib_resource = os.path.join(root, file)
+            if not lib_resource.endswith('.podspec.json'):
+                continue
+            try:
+                with open(lib_resource, "r", encoding="UTF-8") as f:
+                    data = f.read().replace("${", "_{").replace("$(", "_(")
+                    lib_info = json.loads(data)
+                if "name" in lib_info and "version" in lib_info:
+                    lib_name, lib_version = lib_info["name"], str(lib_info["version"])
+                else:
+                    sp = lib_resource.split(os.sep)
+                    lib_name, lib_version = sp[-3], sp[-2]
+                logger.info("update! lib_name: %s, lib_version: %s." % (lib_name, lib_version))
+                base_query = {"name": lib_name, "version": lib_version}
+                lib_source_info.update_one(base_query, {"$set": lib_info}, True)
+            except Exception as e:
+                logger.error("A error occurred while updating lib_source_info: %s" % e.args[0])
 
 
 def main(path, drop, loglevel):
